b665231b97cd0a464769e7fbeb4daf912cce235a
Bohan Chen <bohanchen@brandeis.edu>
2022-03-16 14:48:30 -0400

    bug fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,20 @@
-import { Navbar } from 'react-bootstrap';
+// import { Navbar } from 'react-bootstrap';
 
-export default function NavBar(){
-    return(
-        <Navbar bg="light" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">Spinoza</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Login</Nav.Link>
-        <Nav.Link href="#link">Python IDE</Nav.Link>
+// export default function NavBar(){
+//     return(
+//         <Navbar bg="light" expand="lg">
+//   <Container>
+//     <Navbar.Brand href="#home">Spinoza</Navbar.Brand>
+//     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//     <Navbar.Collapse id="basic-navbar-nav">
+//       <Nav className="me-auto">
+//         <Nav.Link href="#home">Home</Nav.Link>
+//         <Nav.Link href="#link">Login</Nav.Link>
+//         <Nav.Link href="#link">Python IDE</Nav.Link>
         
-      </Nav>
-    </Navbar.Collapse>
-     </Container>
-    </Navbar>
-    )
-}
+//       </Nav>
+//     </Navbar.Collapse>
+//      </Container>
+//     </Navbar>
+//     )
+// }
