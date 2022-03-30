211984abcc83613278079a3fdb6728aa28156f3c
Zheng <czzzzn1001l@gmail.com>
2022-03-30 14:52:58 -0400

    change home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,47 @@
 import NavBar from "./NavBar"
+import { Button, Form } from 'react-bootstrap'
 
 export default function Home() {
-    return(
+    return (
         <>
-            <NavBar/>
-            Home Page Here
-        </>       
+            <NavBar />
+            <div>
+                <p>Home Page Here</p>
+
+                <div>Join Class</div>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Class Pin#</Form.Label>
+                        <Form.Control placeholder="enter class pin" />
+                        <Form.Text className="text-muted">
+                            Please refer to your instructor for pin number
+                        </Form.Text>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Join
+                    </Button>
+                </Form>
+
+                <div>Create New Class</div>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Class Name</Form.Label>
+                        <Form.Control />
+
+                        <Form.Label>Class Code</Form.Label>
+                        <Form.Control placeholder="enter class code"/>
+
+                        <Form.Label>Class Semester</Form.Label>
+                        <Form.Control />
+
+                        <Form.Label>Restrict users email of this class to this domain name (leave it blank if you want any google email to use your class)</Form.Label>
+                        <Form.Control placeholder="enter domain name"/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Create
+                    </Button>
+                </Form>
+            </div>
+        </>
     )
 }
